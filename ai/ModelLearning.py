bd215aa972fc9af73b103c81421324e70758b5dc
HeeSo <heeso02@naver.com>
2021-11-23 16:22:34 +0900

    train/test 데이터셋 Open

--- ai/ModelLearning.py
+++ ai/ModelLearning.py
@@ -19,3 +19,61 @@
 # >> document: 리뷰의 내용
 # >> label: 긍정(1)인지 부정(0)인지 나타냄
 #           평점이 긍정(9~10점), 부정(1~4점), 5~8점은 제거
+
+import json
+import os
+import nltk
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import font_manager, rc
+from pprint import pprint
+from konlpy.tag import Okt
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+
+
+#############
+# File Open #
+#############
+
+# ~.txt 파일에서 데이터를 불러오는 method
+def read_data(filename):
+    with open(filename, 'r', encoding='UTF-8') as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+
+        # data = []
+        # for line in f.read().splitlines():
+        #     data.append(line.split('\t'))
+
+        data = data[1:] # 제목열 제외
+    return data
+
+# nsmc 데이터를 불러와서 python 변수에 담기
+train_data = read_data('./dataset/ratings_train.txt') # 트레이닝 데이터 Open
+test_data = read_data('./dataset/ratings_test.txt')
+
+#'c:\python_workspace\MovieTomatoes\ai\dataset\rating_train.txt' # 절대경로
+
+# / => 하위 폴더
+# .. => 상위 폴더
+# . => 현재 폴더
+
+# ./dataset/ratings_train.txt
+
+# 절대경로와 상대경로
+# C:\python_workspace\MovieTomatoes
+#                           ㄴ ai
+#                                ㄴ dataset
+#                                     ㄴ ratings_text.txt
+#                                     ㄴ ratings_train.txt
+#                           ㄴ model
+
+print(len(train_data))
+print(train_data[0])
+print(train_data[1])
+
+
+
